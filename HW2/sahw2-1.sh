0268fbecd9b08c3e2c9ec2effa65aa2dbcfdcf72
sasaki0487 <bobbytsai040@gmail.com>
2018-10-02 12:21:51 +0800

    2-1 error correct

--- HW2/sahw2-1.sh
+++ HW2/sahw2-1.sh
@@ -1,2 +1,2 @@
-ls -AlR | sort -nrk 5 | awk 'BEGIN{f=0;d=0;s=0;l=0} /^-/ {s+=$5; f++; if(l<6)print ++l":\t"$5"\t"$9} /^d/ {d++} END {printf("DIR NUM : %s\nFILE NUM: %s\nTOTAL SIZE: %s \n",d,f,s)}'
+ls -AlR | sort -nrk 5 | awk 'BEGIN{f=0;d=0;s=0;l=0} /^-/ {s+=$5; f++; if(l<5)print ++l":\t"$5"\t"$9} /^d/ {d++} END {printf("DIR NUM : %s\nFILE NUM: %s\nTOTAL SIZE: %s \n",d,f,s)}'
 
